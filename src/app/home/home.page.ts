e3c5667377b9bff95cd074b47f70d05e80ed3b3e
isFernandes <iago.stosfernandes@gmail.com>
2019-04-11 17:13:46 -0300

    first commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+
+})
+export class HomePage {
+  valor1 = 0;
+  valor2 = 0;
+  resultado = 0;
+  operacao = '';
+  constructor(public navCtrl: NavController) { }
+  onChangeValue(oper) {
+    this.operacao = oper;
+    if (this.operacao === 'menos'){
+      this.resultado = this.valor1 - this.valor2;
+      alert(this.resultado);
+    }
+    if (this.operacao === 'soma') {
+      this.resultado = Number(this.valor1) + Number(this.valor2);
+      alert(this.resultado);
+    }
+
+    if (this.operacao === 'divisao') {
+      this.resultado = this.valor1 / this.valor2;
+      alert(this.resultado);
+    }
+
+    if (this.operacao === 'vezes') {
+      this.resultado = this.valor1 * this.valor2;
+      alert(this.resultado);
+    }
+
+  }
+
+  /*onChangeValue(valor) {
+    if (valor1 == '') {
+      this.valor1 = valor;
+    } else {
+      this.valor2 = valor;
+    }
+
+    if (valor2 != '') {
+      this.Result();
+    }
+  }
+
+  Result() {
+
+  }*/
+  
+}
